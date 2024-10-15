4ac7653c3ef371f0614bb8aaa0136af43cfe0ad9
harshavardhan1726 <harshav1752002@gmail.com>
2024-10-15 16:32:38 +0530

    Adding useEffect for time

--- src/Components/CurrentTime.jsx
+++ src/Components/CurrentTime.jsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 let CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
 
-    let time = new Date()
+  useEffect(() => {
+    const intervalId = setInterval(() => [setTime(new Date())], 1000);
 
-    return <p className="lead">
-        This is the current time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <p className="lead">
+      This is the current time: {time.toLocaleDateString()} -{" "}
+      {time.toLocaleTimeString()}
     </p>
-}
+  );
+};
 
-export default CurrentTime
+export default CurrentTime;
